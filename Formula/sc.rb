5effed8db82e1e15fc8da00bea1ca9b7e858b8e7
Amar Paul <amar.paul16@gmail.com>
2022-03-15 05:37:37 +0000

    sc: add 7.16_1.1.2 bottle.

--- Formula/sc.rb
+++ Formula/sc.rb
@@ -5,6 +5,12 @@ class Sc < Formula
   version "7.16_1.1.2"
   sha256 "1802c9d3d60dac85feb783adf967bc0d2fd7e5f592d9d1df15e4e87d83efcf14"
 
+  bottle do
+    root_url "https://github.com/Amar1729/homebrew-sc/releases/download/sc-7.16_1.1.2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "1f1c52bb1bf52fcd518bdfbe55af72fdf1edf3eaa27cf05e68f30950d1ecf668"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "396886941dd09f02b9b5161b29fabcf8a2c6285de69b0643be39b3de93aa2cb8"
+  end
+
   uses_from_macos "ncurses"
 
   def install
